988bac863fccef82cfe1286132d1899f122e8b92
Mesut Said OZTURK <msozturk12@gmail.com>
2023-02-10 10:10:07 +0300

    Architect Asian Development

---
+++ src/test/java/Options/AppiumTest.java
@@ -0,0 +1,21 @@
+package Options;
+
+import org.junit.Test;
+
+public class AppiumTest {
+    @Test
+    public void NativeAppTest(){
+        System.out.println("Nativetest4");
+        System.out.println("Nativetest5");
+        System.out.println("Nativetest6");
+        System.out.println("Nativetest7");
+    }
+    @Test
+    public void NativeAppTest2(){
+        System.out.println("Nativetest4");
+        System.out.println("Nativetest5");
+        System.out.println("Nativetest6");
+        System.out.println("Nativetest7");
+    }
+
+}
---
+++ src/test/java/Options/RESTAPITEST.java
@@ -0,0 +1,26 @@
+package Options;
+
+import org.junit.Test;
+
+public class RESTAPITEST {
+    @Test
+    public void postJira() {
+
+        System.out.println("postjira1");
+        System.out.println("postjira2");
+        System.out.println("postjira3");
+        System.out.println("postjira4");
+        /*
+        look at me ı have to work...
+         */
+    }
+
+    @Test
+    public void postJira22() {
+
+        System.out.println("postjira1");
+        System.out.println("postjira2");
+        System.out.println("postjira3");
+        System.out.println("postjira4");
+    }
+}
